a48a234d50a85dc123a221db357fc51da7ad9c2e
Mohamed <WeaponZplayz@gmail.com>
2021-08-04 16:15:37 -0500

    finished the first C# challenge

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -6,29 +6,19 @@ namespace ChristmasWithTypes
     {
         static void Main(string[] args)
         {
-            var xmas = new Christmas();
-
-            var xmasDay = Christmas.Day.Thursday;
-
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
-
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
-
-            //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
-
-            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
-            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
-            Console.WriteLine("Here are the presents we would like:");
-
-            foreach (var present in xmas.Presents)
-            {
-                Console.WriteLine($"    {present}");
-            }
-
-            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+        var k = new Christmas();
+        var xmasDay = Christmas.Day.Thursday;
+        k.Santa = "Kris Kringle";
+        k.Presents =  new string[3]{"xbox", "bike", "shoes"};
+        k.TreeHeight = 10;
+        Console.WriteLine($"This year christmas falls on {xmasDay} \n");
+        Console.WriteLine($"Our tree will be {k.TreeHeight} feet high \n");
+        Console.WriteLine("Here are the presents we would like:");
+        foreach (var present in k.Presents)
+        {
+          Console.WriteLine($"    {present}");
+        }
+         Console.WriteLine($"\n We like to call Santa, {k.Santa}");
         }
     }
 }
